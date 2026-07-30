4538cb8aac8485a051c6f5f72f2a98af1a790ca2
teatimesxd <engujunkent@gmail.com>
2026-07-30 16:48:36 +0800

    Kent_30_July

---
+++ app/src/main/java/com/example/energynest/SmartSell.kt
@@ -0,0 +1,20 @@
+package com.example.energynest
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SmartSell() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Your UI elements go here (this is your blank canvas!)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSmartSell() {
+    SmartSell()
+}
